9520cffe4952fc0b1c58310c3ebfa935179e57fe
ben harrington <harringtonben@icloud.com>
2017-09-09 13:32:40 -0500

    this petstore is done

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,11 @@
 console.log("main");
 
 CatStore.catXHR();
+
+document.getElementById("outputdiv").addEventListener("click", (event) => {
+	if (event.target.id.indexOf("adopt") ===0) {
+		// console.log("event", event);
+		let catIndex = event.target.id.split("-")[1];
+		CatStore.adoptCat(catIndex);
+	}
+});
