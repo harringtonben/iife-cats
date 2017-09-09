0e3bc8fe5d882dff478d9294f029321b1b27694b
ben harrington <harringtonben@icloud.com>
2017-09-09 11:07:26 -0500

    dom functions built

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,7 +1,46 @@
-// var CatStore = ((oldCatStore) => {
+var CatStore = ((oldCatStore) => {
 
-	
+	oldCatStore.combineArrays = (cats, owners) => {
+		//combine arrays
+		cats.forEach((cat) => {
+			owners.forEach((owner) => {
+				if (cat.ownerID === owner.id) {
+					cat.owner = owner.name;
+				}
+			});
+		});
+		CatStore.domString(cats);
+	};
+
+	oldCatStore.writeToDom = (inputArray) => {
+		//write some strings
+		let catz = '';
+        for(let i=0; i<inputArray.length; i++){
+            let newCat = "";
+            newCat+=`<div class="col-xs-3">`
+            newCat+=`<div class="catBox col-xs-12 text-center noPad"  id="catBox-${i}">`;
+            newCat+=`<div class="catName">${inputArray[i].name}</div>`;
+            newCat+=`<img class="catImage" src="${inputArray[i].imageUrl}">`;
+            if(inputArray[i].owner !== "None"){
+                newCat+= `<button class="btn btn-default owner">${inputArray[i].owner}</button>`
+            } else {
+                newCat+= `<button class="btn btn-danger owner" id="adopt-${i}">Adopt Me!</button>`
+            }
+            newCat+= `<div class="catSkill text-center">${inputArray[i].specialSkill}</div>`
+            newCat+= `</div>`
+            newCat+= `</div>`
+            catz += newCat;
+        }
+		CatStore.writeToDom(catz);
+	};
+
+	oldCatStore.domString = (strang) => {
+		var outputEl = document.getElementById("outputdiv");
+		outputEl.innerHTML = strang;
+	};
 
 
-// 	return oldCatStore;
-// })(CatStore || {})
+
+
+	return oldCatStore;
+})(CatStore || {})
